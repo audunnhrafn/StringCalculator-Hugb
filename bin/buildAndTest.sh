986e7d68d1aab2ff70d312b22b03b02c23df666a
audunnhrafn <auddi123@gmail.com>
2017-10-12 01:39:19 +0000

    Initial commit

---
+++ bin/buildAndTest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./bin/rm.sh
+./bin/compileCalculator.sh
+./bin/compileTests.sh
+./bin/runTests.sh
---
+++ bin/compileTests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -cp "classes:src/test/resources/junit-4.11.jar"  src/test/java/is/ru/stringcalculator/CalculatorTest.java -d classes/
